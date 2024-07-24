94ebe72af9241d39f2bf284e81a280cb070f1d11
unknown <cs50riyam211@gmail.com>
2024-07-24 11:31:49 +0300

    show model bottom sheet make it responsive

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_local_variable, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key, required this.color1, required this.color2});
@@ -11,38 +14,149 @@ class HomeView extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-          body: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: width < 600
-                  ? Column(
-                      children: [
-                        Container(
-                          width: double.infinity,
-                          height: 200,
-                          color: color1,
-                        ),
-                        Container(
-                            width: double.infinity, height: 200, color: color2),
-                      ],
-                    )
-                  : Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            width: double.infinity,
-                            height: 200,
-                            color: Colors.pink,
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            width: double.infinity,
-                            height: 200,
-                            color: Colors.red,
-                          ),
-                        ),
-                      ],
-                    ))),
+        body: Center(
+          child: Container(
+              width: 100,
+              height: 40,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink.withOpacity(0.3),
+                  ),
+                  onPressed: () {
+                    showModalBottomSheet(
+                        isScrollControlled: true,
+                        useSafeArea: true,
+                        context: context,
+                        builder: (context) => SizedBox(
+                              height: double.infinity,
+                              child: SingleChildScrollView(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: Container(
+                                    width: double.infinity,
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          width: double.infinity,
+                                          height: 60,
+                                          color: const Color.fromARGB(
+                                              255, 197, 134, 130),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Container(
+                                          width: double.infinity,
+                                          height: 60,
+                                          color: const Color.fromARGB(
+                                              255, 190, 101, 131),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Container(
+                                          width: double.infinity,
+                                          height: 60,
+                                          color:
+                                              Color.fromARGB(255, 87, 151, 106),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Container(
+                                          width: double.infinity,
+                                          height: 60,
+                                          color:
+                                              Color.fromARGB(255, 43, 112, 145),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Container(
+                                          width: double.infinity,
+                                          height: 60,
+                                          color:
+                                              Color.fromARGB(255, 207, 84, 27),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        Container(
+                                          width: double.infinity,
+                                          height: 60,
+                                          color:
+                                              Color.fromARGB(255, 250, 246, 44),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ));
+                  },
+                  child: Text(
+                    'click',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ))),
+        ),
+      ),
     );
   }
 }
+
+// todo
+
+class MediaQueryResponsive extends StatelessWidget {
+  const MediaQueryResponsive({
+    super.key,
+    required this.width,
+    required this.color1,
+    required this.color2,
+  });
+
+  final double width;
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: width < 600
+            ? Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    color: color1,
+                  ),
+                  Container(width: double.infinity, height: 200, color: color2),
+                ],
+              )
+            : Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      height: 200,
+                      color: Colors.pink,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ));
+  }
+}
+
+//
